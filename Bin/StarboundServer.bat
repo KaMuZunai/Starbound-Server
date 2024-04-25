c3d5d9b3a3625c54c68e1a81e55e6a946b7461f8
KaMuZunai <87868308+KaMuZunai@users.noreply.github.com>
2024-04-25 21:35:34 +0200

    update

--- Bin/StarboundServer.bat
+++ Bin/StarboundServer.bat
@@ -5,19 +5,20 @@ findstr ""HostLogfileLocation" :" configs\HostLogfileLocation.config > Varg.txt
 set /P varG=<Varg.txt
 del Varg.txt
 set varG=%varG:~28%
+echo [%time%] [StarboundServer] Host: Running Since: %date% %time%.>>%varG%
+echo [%time%] [StarboundServer] Variable: HostLogfileLocation: %varG%>>%varG%
 findstr ""Color" :" configs\CmdWindowColors.config > Vare.txt
 set /P varE=<Vare.txt
 del Vare.txt
 set varE=%varE:~11%
 color %varE%
-echo [StarboundServer] Host: Color Variable: %varE%>>%varG%
-
+echo [%time%] [StarboundServer] Variable: Color: %varE%>>%varG%
 
 :RestartServer
 
 title Starting Starbound Server...
 echo [StarboundServer] Host: Starting Server...
-echo [StarboundServer] Host: Starting Server...>>%varG%
+echo [%time%] [StarboundServer] Host: Starting Server...>>%varG%
 
 findstr ""ServerRestartTimer" :" configs\RestartTimer.config > Vara.txt
 findstr ""BootConfigLocation" :" configs\BootConfigLocation.config > Varb.txt
@@ -40,29 +41,30 @@ set varC=%varC:~24%
 set varD=%varD:~17%
 set varF=%varF:~21%
 
-echo [StarboundServer] Host: BootConfigLocation  Variable: %varB%>>%varG%
-echo [StarboundServer] Host: LogfileLocation Variable: %varC%>>%varG%
-echo [StarboundServer] Host: LogLevel Variable: %varD%>>%varG%
+echo [%time%] [StarboundServer] Variable: BootConfigLocation: %varB%>>%varG%
+echo [%time%] [StarboundServer] Variable: LogfileLocation: %varC%>>%varG%
+echo [%time%] [StarboundServer] Variable: LogLevel: %varD%>>%varG%
 timeout /t 1 >nul
 start "" /min /b %varF% -bootconfig %varB% -loglevel %varD% -logfile %varC%
 timeout /t 15 >nul
 title Starbound Server Running^^!                                                                     
 echo [StarboundServer] Host: Running %varF%                                                                             
-echo [StarboundServer] Host: StarHostVersion Variable: %varF%>>%varG%
+echo [%time%] [StarboundServer] Variable: StarHostVersion: %varF%>>%varG%
 
-echo [StarboundServer] Host: RestartTimer Variable: %varA% seconds.>>%varG%
+echo [%time%] [StarboundServer] Variable: RestartTimer: %varA% seconds.>>%varG%
 for /f %%a in ('copy /Z "%~dpf0" nul') do set "CR=%%a"
 set countdown=%varA%
 
 :TimerLoop
+
 <nul set /p ".=[StarboundServer] Host: Server Automatically Restarting in %countdown% seconds...  !cr!"
 timeout /t 1 /nobreak >nul
 set /a countdown-=1
 if %countdown% GEQ 0 goto :TimerLoop                                                                        
 echo [StarboundServer] Host: Restarting Server...                                                                                                                                                    
-echo [StarboundServer] Host: Restarting Server...>>%varG%
+echo [%time%] [StarboundServer] Host: Restarting Server...>>%varG%
 title Restarting Server...
 timeout /t 1 >nul
 taskkill /IM %varF% /F >nul
-echo [StarboundServer] Host: Starbound Closed.>>%varG%
+echo [%time%] [StarboundServer] Host: Starbound Closed.>>%varG%
 goto RestartServer
